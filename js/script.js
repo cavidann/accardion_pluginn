3617ab58d366aad854b2c5a3e58de9e0fe4b39c1
cavidann <cavi98@icloud.com>
2017-04-26 23:54:05 -1100

    accardion_pluginn

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+$.fn.accardion=function(){
+    
+    var accardion =$(".item");
+    $(".head").click(function(ev){
+        $(this).parent().find(".body").slideToggle();
+        $(this).find("i").toggleClass("fa-minus");
+           $(".head").not($(this)).parent().find(".body").slideUp().find("i").removeClass("fa-minus").addClass("fa-plus");
+    })
+}
+$(document).ready(function(){
+    $(".head").accardion();
+})
+
+
+
+
+
+
