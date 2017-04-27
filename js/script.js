fb5292f9f9f9a0694054505efd0ea0a95a23cde0
cavidann <cavi98@icloud.com>
2017-04-26 23:55:40 -1100

    accardion_pluginn

--- js/script.js
+++ js/script.js
@@ -5,7 +5,8 @@ $.fn.accardion=function(){
     $(".head").click(function(ev){
         $(this).parent().find(".body").slideToggle();
         $(this).find("i").toggleClass("fa-minus");
-           $(".head").not($(this)).parent().find(".body").slideUp().find("i").removeClass("fa-minus").addClass("fa-plus");
+           $(".head").not($(this)).parent().find(".body").slideUp();
+           $(".head").not($(this)).find("i").removeClass("fa-minus").addClass("fa-plus");
     })
 }
 $(document).ready(function(){
